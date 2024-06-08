0514504f8a9b515a7e75e457e5e45bbd4b7ef8e0
vishwas <vishwas101kumar@gmail.com>
2024-06-09 02:11:15 +0530

    Implemented basic profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,8 @@
 import { BiLeftArrowAlt } from "react-icons/bi";
-import { BANNER_URL } from "../utils/constants";
+import { BANNER_URL, SAMPLE_URL } from "../utils/constants";
 const Profile = () => {
   return (
-    <div className="border border-red-200 font-montserrat">
+    <div className="font-montserrat">
       <div className="border-b flex items-center gap-x-4 p-2">
         <div>
           <BiLeftArrowAlt className="text-2xl" />
@@ -13,7 +13,24 @@ const Profile = () => {
         </div>
       </div>
       <div>
-        <img src={BANNER_URL} className="w-full" />
+        <img src={BANNER_URL} alt="banner" className="w-full" />
+      </div>
+      <div className="flex justify-between items-center relative px-4 ">
+        <div className="w-[150px] h-[150px] border-4 border-white rounded-full overflow-hidden -mt-20">
+          <img src={SAMPLE_URL} alt="profilImage" className="object-contain" />
+        </div>
+        <div className="">
+          <button className="bg-black text-white text-xs px-4 py-2 rounded-full cursor-pointer">
+            Follow
+          </button>
+        </div>
+      </div>
+
+      <div className="p-2 px-4">
+        <h1 className="font-bold text-lg">Vishwas Kumar</h1>
+        <h3 className="text-xs text-gray-600">@Vishwas123</h3>
+        <p className="mt-4 text-sm">Student at LPU</p>
+        <p className="text-sm">A passionate Fullstack developer from india</p>
       </div>
     </div>
   );
