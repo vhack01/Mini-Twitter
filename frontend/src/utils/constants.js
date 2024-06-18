13fc5c4b58cba601a6bb505de5b154cc00a89832
vishwas <vishwas101kumar@gmail.com>
2024-06-18 10:09:21 +0530

    add origin in cors

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -7,5 +7,7 @@ export const BANNER_URL =
 export const LOGO =
   "https://i.pinimg.com/originals/73/db/02/73db02344af2f927fd361decc424e8b8.png";
 
-export const USER_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_END_POINT =
+  "https://mini-twitter-server.vercel.app/api/v1/user";
+export const TWEET_END_POINT =
+  "https://mini-twitter-server.vercel.app/api/v1/tweet";
