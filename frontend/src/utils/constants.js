e72b1f97eaf0d2e1877cc4ff8aac43494b2a7ce7
vishwas <vishwas101kumar@gmail.com>
2024-06-19 23:22:50 +0530

    Redirected user to login page after successfull signup

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -9,6 +9,7 @@ export const LOGO =
 
 // http://localhost:8080
 
-export const USER_END_POINT = "https://mini-twitter-app.vercel.app/api/v1/user";
+export const USER_END_POINT =
+  "https://mini-twitter-server.vercel.app/api/v1/user";
 export const TWEET_END_POINT =
-  "https://mini-twitter-app.vercel.app/api/v1/tweet";
+  "https://mini-twitter-server.vercel.app/api/v1/tweet";
